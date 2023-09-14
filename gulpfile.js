c3b410b0759d6ac7b855a557359521c7d2ade59e
Aleks Molnar <aleks.molnar02@gmail.com>
2023-09-14 10:03:20 +0200

    9. nap

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,33 @@
-const { src, dest, watch } = require("gulp");
-const sass = require("gulp-sass");
+const gulp = require("gulp");
+const sass = require("gulp-sass")(require("sass"));
+const browserSync = require("browser-sync").create();
 
-sass.compiler = require("sass"); // Use the "sass" package as the Sass compiler
+// Compile scss into css
+function style() {
+  //.1 Where is my scss file
+  return (
+    gulp
+      .src("./scss/**/*.scss")
+      // 2. Pass that file through Sass compiler
+      .pipe(sass().on("error", sass.logError))
+      // 3. Where do I save the compiled CSS
+      .pipe(gulp.dest("./css"))
+      // 4. Stream changes to all browsers
+      .pipe(browserSync.stream())
+  );
+}
 
-const compileSass = () => {
-  return src("./scss/*.scss")
-    .pipe(sass().on("error", sass.logError))
-    .pipe(dest("./css/"));
-};
+// Define the watch task
+function watch() {
+  browserSync.init({
+    server: {
+      baseDir: "./",
+    },
+  });
+  gulp.watch("./scss/**/*.scss", style);
+  gulp.watch("./*.html").on("change", browserSync.reload);
+  gulp.watch("./js/**/*.js").on("change", browserSync.reload);
+}
 
-const devWatch = () => {
-  watch("./scss/**/*.scss", compileSass);
-};
-
-exports.compileSass = compileSass;
-exports.devWatch = devWatch;
+exports.style = style;
+exports.watch = watch;
